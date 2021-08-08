7d9fd9ed4bfe263b0d0bda395995558abba7f80a
Carlos Gonzalez Juarez <carlosgj94@gmail.com>
2021-08-08 20:43:22 +0100

    Updating the contract so only people that participated can claim

--- contracts/PartyBidPunk2066.sol
+++ contracts/PartyBidPunk2066.sol
@@ -2,39 +2,35 @@
 pragma solidity ^0.8.5;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IPartyBid.sol";
 
 contract PartyBidPunk2066 is ERC721URIStorage, Ownable {
   uint256 public tokenCounter;
   string public internalTokenURI;
-  IERC20 public deadToken;
+  IPartyBid public deadToken;
   mapping(address => bool) public minted;
 
   constructor(string memory _tokenURI, address _deadAddress)
-    ERC721("DeadTokenNFT", "DTN")
+    ERC721("DeadPunkPartyBid", "DPPB")
   {
     tokenCounter = 0;
     internalTokenURI = _tokenURI;
-    deadToken = IERC20(_deadAddress);
+    deadToken = IPartyBid(_deadAddress);
   }
 
-  function mintCollectible() public returns (uint256) {
-    // require(tokenCounter < 1000, "All tokens already minted");
-    require(deadToken.balanceOf(msg.sender) > 0, "Owner doesn't have DEAD");
+  function mintCollectible() public {
+    require(deadToken.totalContributed(msg.sender) > 0, "Owner didn't contributed in the PartyBid");
     require(minted[msg.sender] == false, "NFT already minted");
     minted[msg.sender] = true;
 
     uint256 newTokenId = tokenCounter;
+    tokenCounter = tokenCounter + 1;
     _safeMint(msg.sender, newTokenId);
     _setTokenURI(newTokenId, internalTokenURI);
-
-    tokenCounter = tokenCounter + 1;
-
-    return newTokenId;
   }
 
   function hasBalance() public view returns (bool) {
-    return deadToken.balanceOf(msg.sender) > 0;
+    return deadToken.totalContributed(msg.sender) > 0;
   }
 }
